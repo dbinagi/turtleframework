f3d303bc226e119e92f7f06d2176e3b1d37a0707
Diego <diegobinagi@gmail.com>
2020-05-13 13:22:08 -0300

    Framework test

---
+++ Runtime/CNewCoolClass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TurtleGames.Framework.Runtime
+{
+    public class CNewCoolClass
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public string GetMessage(){
+            return "Hola Diego, buen sdfsdf";
+        }
+    }
+}
