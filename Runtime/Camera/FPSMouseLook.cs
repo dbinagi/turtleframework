dfedfee22b8591f6908de3615fb0dfe96eb6fc0c
Diego <diegobinagi@gmail.com>
2021-05-17 23:30:24 -0300

    Add FPS Mouse Look

---
+++ Runtime/Camera/FPSMouseLook.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+
+namespace TurtleGames.Framework.Runtime.Camera
+{
+    [RequireComponent(typeof(UnityEngine.Camera))]
+    public class FPSMouseLook : MonoBehaviour
+    {
+        [SerializeField]
+        private CursorLockMode lockMode;
+
+        [SerializeField]
+        private float mouseSensitivity;
+
+        [Tooltip("Lerp Smooth for the axis X rotation")]
+        [SerializeField]
+        private float rotationXSmooth;
+
+        [Tooltip("Lerp Smooth for the axis Y rotation")]
+        [SerializeField]
+        private float rotationYSmooth;
+
+        [Tooltip("Set to True to block all rotations")]
+        private bool blockMovement;
+
+        [Tooltip("Limit of the camera when looking at the floor related to the horizon")]
+        [SerializeField]
+        private float lookAtFloorRotationLimit;
+
+        [Tooltip("Limit of the camera when looking at the ceiling related to the horizon")]
+        [SerializeField]
+        private float lookAtCeilingRotationLimit;
+
+        public bool BlockMovement { get => blockMovement; set => blockMovement = value; }
+        public CursorLockMode LockMode { get => lockMode; set => lockMode = value; }
+
+        #region Unity Functions
+
+        void Start()
+        {
+            Cursor.lockState = LockMode;
+        }
+
+        void Update()
+        {
+            if (BlockMovement)
+                return;
+
+            RotateY();
+            RotateX();
+        }
+
+        #endregion
+
+        #region Private Functions
+
+        void RotateY()
+        {
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime * -1;
+            Quaternion rotation = Quaternion.Euler(mouseY, 0, 0) * transform.localRotation;
+
+            Quaternion newRotation = Quaternion.Lerp(transform.localRotation, rotation, Time.deltaTime * rotationYSmooth);
+
+            if (newRotation.eulerAngles.z != 0)
+                return;
+
+            bool applyRotation = false;
+
+            // Look down quadrant
+            if(newRotation.eulerAngles.x >= 0 && newRotation.eulerAngles.x <= 90)
+            {
+                if (newRotation.eulerAngles.x <= lookAtFloorRotationLimit)
+                    applyRotation = true;
+            // Look up quadrant
+            } else if(newRotation.eulerAngles.x >= 270 && newRotation.eulerAngles.x <= 360)
+            {
+                if (newRotation.eulerAngles.x >= 360 - lookAtCeilingRotationLimit)
+                    applyRotation = true;
+            }
+
+            if (applyRotation)
+                transform.localRotation = newRotation;
+        }
+
+        void RotateX()
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            Quaternion newRotation = Quaternion.AngleAxis(mouseX, Vector3.up) * transform.parent.rotation;
+            transform.parent.rotation = Quaternion.Lerp(transform.parent.rotation, newRotation, Time.deltaTime * rotationXSmooth);
+        }
+
+        #endregion
+    }
+}
---
+++ Runtime/Camera/README.md
@@ -0,0 +1,13 @@
+## How to use
+
+### FPSMouseLook
+
+**Description: ** Move Camera with mouse movement.
+
+**How to use it: **
+
+Use this script in the GameObject that has the camera
+
+**Configuration: **
+
+Set lockMode to Locked if you want the cursor to disappear
